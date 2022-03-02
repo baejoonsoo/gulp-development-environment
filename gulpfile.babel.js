f1285e391278494cd28c036bf2a3a161c5edbe4d
BaeJoonSoo <baejoonsoo21@gmail.com>
2022-03-02 21:49:26 +0900

    Feat: build clean func

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,5 +1,6 @@
 import gulp from 'gulp';
 import gpug from 'gulp-pug';
+import del from 'del';
 
 const routes = {
   pug: {
@@ -11,4 +12,6 @@ const routes = {
 export const pug = () =>
   gulp.src(routes.pug.src).pipe(gpug()).pipe(gulp.dest(routes.pug.dest));
 
-export const dev = gulp.series([pug]);
+export const clean = () => del(['build']);
+
+export const dev = gulp.series([clean, pug]);
